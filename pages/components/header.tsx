567cd359d2437beca9b18cf3d8abd27ac890e948
Anton <62949848+icepaq@users.noreply.github.com>
2022-08-18 13:05:07 -0400

    init

---
+++ pages/components/header.tsx
@@ -0,0 +1,85 @@
+import type { NextPage } from 'next'
+import styles from '../../styles/Header.module.css'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+const Header: NextPage = () => {
+
+    const [windowRatio, setWindowRatio] = useState(0);
+
+    const router = useRouter();
+
+    useEffect(() => {
+        setWindowRatio(window.innerWidth / window.innerHeight);
+        window.addEventListener('resize', updateSize);
+    }, [])
+
+    function updateSize() {
+        setWindowRatio(window.innerWidth / window.innerHeight);
+    }
+
+    const openMenu = () => {
+        const menu = document.getElementsByClassName(styles.mobileheader)[0] as HTMLElement;
+        menu.style.width = '60%';
+        menu.style.padding = '10px 10px';
+    }  
+
+    const closeMenu = () => {
+        const menu = document.getElementsByClassName(styles.mobileheader)[0] as HTMLElement;
+        menu.style.width = '0';
+        menu.style.padding = '10px 0px';
+    }
+
+    const logout = async () => {
+        router.reload();
+    }
+
+    const deskTopHeader = 
+    <>
+        <div className={styles.desktopheader}>
+            <div className={styles.logoheader}>
+                <Image src="/logo.png" alt="logo" width={150} height={40} /> 
+            </div>
+            
+            <div className={styles.menuitems}>
+                <div className={styles.desktopmenuitem}><Link href="/">Home</Link></div>
+                <div className={styles.desktopmenuitem}><Link href="/">Catalogue</Link></div>
+                <div className={styles.desktopmenuitem}><Link href="/">About Us</Link></div>
+                <div className={styles.desktopmenuitem}><Link href="/">My Account</Link></div>
+            </div>
+
+            <div className={styles.login} >
+                <div className={styles.loginbutton}>Login</div>
+            </div>
+        </div>
+    </>
+
+    const mobileHeader = 
+    <>
+        <div className={styles.menuButton} onClick={openMenu}>
+            <Image src='/menu.png' alt='menu' width={20} height={20} />
+        </div>
+        <div className={styles.mobileheader}>
+            <span className={styles.imagewrapper}>
+                <Image src="/logo.png" alt="logo" width={80} height={20} />
+            </span>
+            <div className={styles.mobileX} onClick={closeMenu}>x</div>
+            <div className={styles.mobilemenuitems}>
+                <div className={styles.mobilemenuitem}><Link href="/">Home</Link></div>
+                <div className={styles.mobilemenuitem}><Link href="/">Catalogue</Link></div>
+                <div className={styles.mobilemenuitem}><Link href="/">About Us</Link></div>
+                <div className={styles.mobilemenuitem}><Link href="/">My Account</Link></div>
+            </div>
+        </div>
+    </>
+
+    if(windowRatio == 0) {
+        return <></>
+    }
+
+    return windowRatio > 1.3 ? deskTopHeader : mobileHeader;
+}
+
+export default Header
---
+++ pages/index.tsx
@@ -0,0 +1,91 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from './components/header'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <Header />
+            <div className={styles.body}>
+                <div className={styles.banner}>
+                    <div className={styles.bannerTextWrapper}>
+                        <div className={styles.bannerTitle}>
+                            Find Your Next Scent
+                        </div>
+                        <div className={styles.bannerSubtitle}>
+                            Discover your favorite fragrance. Starting at $5 / month.
+                        </div>
+                        <div className={styles.bannerButtonWrapper}>
+                            <div className={styles.bannerButton} role={'button'}>
+                                Get Started
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.howItWorks}>
+                    <div className={styles.title}>
+                        How it works
+                    </div>
+                    <div className={styles.row}>
+                        <div className={styles.item}>
+                            <div className={styles.itemTitle}>
+                                Step 1
+                            </div>
+                            <div className={styles.itemContent}>
+                                Take a 2 minute quiz to we know what products to recommend
+                            </div>
+                        </div>
+                        <div className={styles.item}>
+                            <div className={styles.itemTitle}>
+                                Step 2
+                            </div>
+                            <div className={styles.itemContent}>
+                                See your recommendations and subscribe
+                            </div>
+                        </div>
+                        <div className={styles.item}>
+                            <div className={styles.itemTitle}>
+                                Step 3
+                            </div>
+                            <div className={styles.itemContent}>
+                                Get your monthly scents. You will also have the choice to modify your subscription
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.favoriteBrands}>
+                    <div className={styles.title}>Your Favorite Brands</div>
+                    <div className={styles.text}>
+                        We carry all of your favorite brands and products. Don't see what you like? Let us know by sending an email
+                        to products@scentsubs.com
+                    </div>
+                    <div className={styles.brandBanner}>
+                        <Image src={'/brandbanner.png'} width={3000} height={1000} />
+                        A complete list of available products can be found here
+                    </div>
+                </div>
+                <div className={styles.footer}>
+                    <div className={styles.column}>
+                        <div className={styles.columnTitle}>
+                            Catalogue
+                        </div>
+                        <div className={styles.columnContent}>
+                            <Link href={'/catalogue'}>Cologne</Link>
+                        </div>
+                        <div className={styles.columnContent}>
+                            <Link href={'/catalogue'}>Perfume</Link>
+                        </div>
+                        <div className={styles.columnContent}>
+                            <Link href={'/catalogue'}>Other Fragrances</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
