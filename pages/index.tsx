37bec19fc6832882fa695e8fa6e12f2c9a44e03c
Anton <62949848+icepaq@users.noreply.github.com>
2022-08-22 13:07:00 -0400

    footer updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -99,6 +99,20 @@ const Home: NextPage = () => {
                                 <Link href={'https://scent-subs-careers.notion.site/Scent-Subs-ae462695a456476d8987317fd9ae8955'}>Careers</Link>
                             </div>
                         </div>
+                        <div className={styles.column}>
+                            <div className={styles.columnTitle}>
+                                Stay Connected
+                            </div>
+                            <div className={styles.columnContent}>
+                                <Link href={'/catalogue'}>Instagram</Link>
+                            </div>
+                            <div className={styles.columnContent}>
+                                <Link href={'/catalogue'}>Twitter</Link>
+                            </div>
+                            <div className={styles.columnContent}>
+                                <Link href={'https://scent-subs-careers.notion.site/Scent-Subs-ae462695a456476d8987317fd9ae8955'}>Newsletter</Link>
+                            </div>
+                        </div>
                     </div>
                     <div className={styles.footerrow}>
                         <div className={styles.copyright}>ScentSubs 2022. Proudly Canadian.</div>
